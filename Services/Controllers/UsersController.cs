113e1db07b34ddceb669be6f7ca59507738df0ac
Kalen Arendt <dev.KalenArendt@gmail.com>
2024-07-20 23:45:53 -0500

    [setup-services]: Added UsersController.cs

---
+++ Services/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Services.Data;
+using Services.Models;
+
+namespace Services.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController(DataContext context) : ControllerBase
+{
+	[HttpGet] // /api/users
+	public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+	{
+		context.Entry
+		
+		return await context.Users.ToListAsync();
+	}
+
+	[HttpGet("{id:int}")] // /api/users/3
+	public async Task<ActionResult<AppUser>> GetUser(int id)
+	{
+		var user = await context.Users.FindAsync(id);
+		
+		if (user == null)
+		{
+			return NotFound();
+		}
+
+		return user;
+	}
+}
