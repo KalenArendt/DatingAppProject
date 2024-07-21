63e5a03c8477431912b7d80594d9e48dccc321d6
Kalen Arendt <dev.KalenArendt@gmail.com>
2024-07-21 13:46:16 -0500

    [setup-angular]: made http request in frontend

--- DatingApp/src/app/app.component.ts
+++ DatingApp/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppUser } from './models/app-user';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,42 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  http = inject(HttpClient);
+
   title = 'DatingApp';
+
+  // users!: AppUser;
+  users: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // this.http.get<AppUser>('https://localhost:5001/services/users').subscribe({
+    // 	next: (response: AppUser) => {
+    // 		this.users = response;
+    // 		console.log(response);
+    // 	},
+    // 	error: error => {
+    // 		console.log(error);
+    // 	},
+    // 	complete: () => {
+    // 		console.log('complete')
+    // 	}
+    // });
+
+    this.http.get('https://localhost:5001/services/users').subscribe({
+      next: (response) => {
+        this.users = response;
+        console.log(response);
+      },
+      error: error => {
+        console.log(error);
+      },
+      complete: () => {
+        console.log('complete')
+      }
+    });
+  }
 }
