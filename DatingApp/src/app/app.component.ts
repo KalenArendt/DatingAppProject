e8e776d675f5f18f21f8c39f72a1627e783b4370
Kalen Arendt <dev.KalenArendt@gmail.com>
2024-07-21 20:16:35 -0500

    [setup-angular]: installed ngx-bootstrap, bootstrap, and font-awesome

--- DatingApp/src/app/app.component.ts
+++ DatingApp/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppUser } from './models/app-user';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -16,27 +17,13 @@ export class AppComponent implements OnInit {
 
   title = 'DatingApp';
 
-  // users!: AppUser;
-  users: any;
+  users: AppUser[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
-    // this.http.get<AppUser>('https://localhost:5001/services/users').subscribe({
-    // 	next: (response: AppUser) => {
-    // 		this.users = response;
-    // 		console.log(response);
-    // 	},
-    // 	error: error => {
-    // 		console.log(error);
-    // 	},
-    // 	complete: () => {
-    // 		console.log('complete')
-    // 	}
-    // });
-
-    this.http.get('https://localhost:5001/services/users').subscribe({
-      next: (response) => {
+    this.http.get<AppUser[]>('https://localhost:5001/services/users').subscribe({
+      next: (response: AppUser[]) => {
         this.users = response;
         console.log(response);
       },
